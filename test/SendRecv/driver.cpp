089e0e9fdb56c550b49cc6d96ef8eac13f1a6951
Michel Schanen <schanen@stce.rwth-aachen.de>
2014-01-08 17:51:08 +0100

    Towards a testing suite

---
+++ test/SendRecv/driver.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <mpi.h>
+
+using namespace std;
+
+void comp(double *x, double &y, int &n) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  double *buf=new double[n];
+  y=0;
+  if(rank==0) {
+    for(int i=0;i<n;i++) x[i]=x[i]*x[i];
+    MPI_Send(x,n,MPI_DOUBLE,1,0,MPI_COMM_WORLD);
+    MPI_Recv(buf,n,MPI_DOUBLE,1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    for(int i=0;i<n;i++) {
+      y+=buf[i];
+    }
+  }
+  if(rank==1) {
+    MPI_Recv(buf,n,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    for(int i=0;i<n;i++) { 
+      buf[i]=sin(buf[i]);
+    }
+    MPI_Send(buf,n,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+  }
+  delete [] buf;
+}
+
+
+int main(int argc, char *argv[]) {
+  MPI_Init(&argc, &argv);
+  double h=1e-6;
+  if(argc<2) {
+    cout << "Not enough arguments. Missing problem size." << endl;
+    MPI_Finalize();
+    return 0;
+  }
+  int numprocs;
+  MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+  if(numprocs!=2) {
+    cout << "Must be run with number of processes set to 2." << endl;
+    MPI_Finalize();
+    return 0;
+  }
+  int n=atoi(argv[1]);
+  cout << "Problem size: " << n << endl;
+  double *x=new double[n];
+  double y=0;
+  for(int i=0;i<n;i++) x[i]=(double) i;
+  comp(x,y,n);
+  cout << "Result:" << y << endl;
+  MPI_Finalize();
+  delete [] x;
+  return 0;
+}
