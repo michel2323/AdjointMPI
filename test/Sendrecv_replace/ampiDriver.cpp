eca149b6907b56071542c9a1ea51374ae93caa6f
Michel Schanen <schanen@stce.rwth-aachen.de>
2014-01-09 14:47:13 +0100

    Sendrecv_replace test

---
+++ test/Sendrecv_replace/ampiDriver.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include "dco.hpp"
+#include "ampi_tape.hpp"
+
+using namespace std;
+
+typedef dco::a1s::type active;
+
+void comp(active *x, active &y, int &n) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  active *buf=new active[n];
+  y=0;
+  if(rank==0) {
+    for(int i=0;i<n;i++) x[i]=x[i]*x[i];
+    AMPI_Sendrecv_replace(x,n,MPI_DOUBLE,1,0,1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    for(int i=0;i<n;i++) {
+      y+=x[i];
+    }
+  }
+  if(rank==1) {
+    AMPI_Recv(buf,n,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    for(int i=0;i<n;i++) { 
+      buf[i]=sin(buf[i]);
+    }
+    AMPI_Send(buf,n,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+  }
+  delete [] buf;
+}
+
+
+int main(int argc, char *argv[]) {
+  AMPI_Init(&argc, &argv);
+  dco::a1s::global_tape = dco::a1s::tape::create(1e4);
+  active h=1e-6;
+  if(argc<2) {
+    cout << "Not enough arguments. Missing problem size." << endl;
+    AMPI_Finalize();
+    return 0;
+  }
+  int numprocs,rank;
+  MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  if(numprocs!=2) {
+    cout << "Must be run with number of processes set to 2." << endl;
+    AMPI_Finalize();
+    return 0;
+  }
+  int n=atoi(argv[1]);
+  cout << "Problem size: " << n << endl;
+  active *x=new active[n];
+  active y=0;
+  for(int i=0;i<n;i++) x[i]=(active) i;
+  if(rank==0) {
+    for(int i=0;i<n;i++)
+      dco::a1s::global_tape->register_variable(x[i]);
+  }
+  active *x_saved=new active[n];
+  for(int i=0;i<n;i++) x_saved[i]=x[i];
+  comp(x,y,n);
+  cout << "Result:" << y << endl;
+  cout << "Derivatives:" << endl;
+  if(rank == 0) dco::a1s::set(y, 1., -1);
+  if(rank == 1) dco::a1s::set(y, 0., -1);
+  dco::a1s::global_tape->interpret_adjoint();
+  //if(rank == 0) {
+    double g=0;
+    for(int i=0;i<n;i++) {
+      dco::a1s::get(x_saved[i], g, -1);
+      cout << g << endl;
+    }
+    //}
+    
+  AMPI_Finalize();
+  delete [] x; delete [] x_saved;
+  return 0;
+}
---
+++ test/Sendrecv_replace/fdDriver.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <mpi.h>
+
+using namespace std;
+
+void comp(double *x, double &y, int &n) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  double *buf=new double[n];
+  y=0;
+  if(rank==0) {
+    for(int i=0;i<n;i++) x[i]=x[i]*x[i];
+    MPI_Sendrecv_replace(x,n,MPI_DOUBLE,1,0,1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    for(int i=0;i<n;i++) {
+      y+=x[i];
+    }
+  }
+  if(rank==1) {
+    MPI_Recv(buf,n,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    for(int i=0;i<n;i++) { 
+      buf[i]=sin(buf[i]);
+    }
+    MPI_Send(buf,n,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+  }
+  delete [] buf;
+}
+
+
+int main(int argc, char *argv[]) {
+  MPI_Init(&argc, &argv);
+  double h=1e-6;
+  if(argc<2) {
+    cout << "Not enough arguments. Missing problem size." << endl;
+    MPI_Finalize();
+    return 0;
+  }
+  int numprocs;
+  MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+  if(numprocs!=2) {
+    cout << "Must be run with number of processes set to 2." << endl;
+    MPI_Finalize();
+    return 0;
+  }
+  int n=atoi(argv[1]);
+  cout << "Problem size: " << n << endl;
+  double *x=new double[n];
+  double y=0;
+  for(int i=0;i<n;i++) x[i]=(double) i;
+  double *x_saved=new double[n];
+  for(int i=0;i<n;i++) x_saved[i]=x[i];
+  comp(x,y,n);
+  cout << "Result:" << y << endl;
+  cout << "Derivatives:" << endl;
+  double y_saved=y;
+  for(int i=0;i<n;i++) {
+    for(int j=0;j<n;j++) x[j]=x_saved[j];
+    x[i]=x[i]+h;
+    comp(x,y,n);
+    cout << (y-y_saved)/h << endl;
+  }
+  MPI_Finalize();
+  delete [] x; delete [] x_saved;
+  return 0;
+}
